416f9d278a20088addffca25fda6a04c42e17293
galahirudilhara <galahirudilhara@gmail.com>
2024-05-04 23:42:36 +0530

    created the RatesDAOImpl.java class

---
+++ src/test/java/com/getposted/model/RatesDAOImplTest.java
@@ -0,0 +1,35 @@
+package com.getposted.model;
+
+import org.junit.Test;
+
+public class RatesDAOImplTest {
+    @Test
+    public void testDelete() {
+
+    }
+
+    @Test
+    public void testGet() {
+
+    }
+
+    @Test
+    public void testGet2() {
+
+    }
+
+    @Test
+    public void testGetAll() {
+
+    }
+
+    @Test
+    public void testInsert() {
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+    }
+}
