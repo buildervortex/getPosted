929e95deacdd6850e94f7d9c72829216329eb472
galahirudilhara <galahirudilhara@gmail.com>
2024-05-05 10:15:13 +0530

    created the HasDAOImpl class

--- src/main/java/com/getposted/model/HasDAO.java
+++ src/main/java/com/getposted/model/HasDAO.java
@@ -1,5 +1,8 @@
 package com.getposted.model;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface HasDAO extends DAO<Has>{
-    
+    List<Has> getList(int authorId) throws SQLException;
 }
